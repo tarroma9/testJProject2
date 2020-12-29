addbf7da0ebbfc6e789c00ba49d540dc0bb196ba
Roman <test@test.test>
2020-12-29 20:13:17 +0300

    1. First prctc

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
 // "use strict";
 
-const isChecked = false,
-    isClose = false;
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-console.log(isChecked || !isClose);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},   // Т.к. это объект, то мы легко сможем задавать ему ключ -> значение. 
+    actors: {},   
+    genres: [],  // В массиве при этом лишь порядковый номер
+    private: false
+};
 
+
+
+const movie1 = prompt("Напишите один из последних просмотренных фильмов", ""),
+      rating1 = prompt("На сколько вы его оцениваете?", ""),
+      movie2 = prompt("Напишите один из последних просмотренных фильмов", ""),
+      rating2 = prompt("На сколько вы его оцениваете?", "");
+
+personalMovieDB.movies[movie1] = rating1;
+personalMovieDB.movies[movie2] = rating2; 
+
+console.log(personalMovieDB);
